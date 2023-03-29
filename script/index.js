2dc34ef623f0cb4c63debec04f6c185eea706832
Moumita <moupriya2609@gmail.com>
2023-03-29 21:32:11 +0530

    rmv

--- script/index.js
+++
@@ -1,9 +0,0 @@
- localStorage.getItem("user-name", current_user.name)
- let nameShow = document.getElementById("userN")
- let logoutbtn = document.getElementById("logoutBtn")
-
- function logout() {
-     localStorage.removeItem('user-name');
-     localStorage.removeItem('user-email');
-     logoutbtn.innerHTML = null
- }
