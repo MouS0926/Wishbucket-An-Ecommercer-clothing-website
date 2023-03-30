ea54f877c69c08135b60949356fd73b86080ccf9
Moumita <moupriya2609@gmail.com>
2023-03-30 23:23:35 +0530

    productshow-search-sorting-cart

---
+++ script/cart.js
@@ -0,0 +1,91 @@
+let LSdata = JSON.parse(PlocalStorage.getItem("product")) || []
+let productRow = document.getElementById("product-row")
+displayProduct(productData)
+
+let allCheckboxes = document.querySelectorAll('input[type=checkbox]');
+
+// sort by price
+let sortprice = document.getElementById("sortprice")
+sortprice.addEventListener("change", function() {
+
+
+    if (sortprice.value == "") {
+        displayProduct(productData)
+    }
+
+    if (sortprice.value == "htl") {
+        productData.sort((a, b) => {
+            return b.price - a.price
+        })
+        displayProduct(productData)
+    }
+
+    if (sortprice.value == "lth") {
+        productData.sort((a, b) => {
+            return a.price - b.price
+        })
+        displayProduct(productData)
+    }
+
+
+
+})
+
+
+// sort by price
+
+function displayProduct(data) {
+    productRow.innerHTML = ""
+
+    let totalProd = document.getElementById("total-product")
+    totalProd.innerText = data.length
+
+
+    data.filter((ele) => {
+            return ele.cat == "woman"
+        })
+        .forEach(function(el) {
+            let prodCol = document.createElement("div")
+            prodCol.setAttribute("class", "col-md-4")
+
+            let card = document.createElement("div")
+            card.setAttribute("class", "card")
+
+            let image = document.createElement("img")
+            let brand = document.createElement("h5")
+            let title = document.createElement("p")
+            let category = document.createElement("p")
+            let price = document.createElement("p")
+            let cartBtn = document.createElement("button")
+            cartBtn.setAttribute("class", "btn btn-outline-primary")
+            cartBtn.setAttribute("type", "button")
+
+            image.setAttribute("src", el.image)
+            brand.innerText = el.brand
+            title.innerText = el.title
+            category.innerText = el.category
+            price.innerText = el.price
+            cartBtn.innerText = "Add to Cart"
+
+            productRow.append(prodCol)
+            prodCol.append(card)
+            card.append(image, brand, title, category, price, cartBtn)
+
+            localStorage.setItem("product", JSON.stringify(productData))
+
+        })
+
+
+}
+
+let searchIn = document.getElementById("search")
+searchIn.addEventListener("input", function() {
+    let filtered = productData.filter(function(el, i) {
+        if (el.title.toUpperCase().includes(searchIn.value.toUpperCase()) == true) {
+            return true
+        } else {
+            return false
+        }
+    })
+    displayProduct(filtered)
+})
--- script/index.js
+++ script/index.js
@@ -1,9 +1,11 @@
- localStorage.getItem("user-name", current_user.name)
+ let Cname = localStorage.getItem("user-name")
+
  let nameShow = document.getElementById("userN")
  let logoutbtn = document.getElementById("logoutBtn")
+
 
  function logout() {
      localStorage.removeItem('user-name');
      localStorage.removeItem('user-email');
-     logoutbtn.innerHTML = null
+     //  logoutbtn.innerHTML = null
  }
